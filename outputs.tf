79ca0380dff2ae56361d1548e572d5d8ceae615b
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:40 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,33 @@
-output "machine_learning_synapse_sparks" {
-  description = "All machine_learning_synapse_spark resources"
-  value       = azurerm_machine_learning_synapse_spark.machine_learning_synapse_sparks
-}
 output "machine_learning_synapse_sparks_description" {
-  description = "List of description values across all machine_learning_synapse_sparks"
-  value       = [for k, v in azurerm_machine_learning_synapse_spark.machine_learning_synapse_sparks : v.description]
+  description = "Map of description values across all machine_learning_synapse_sparks, keyed the same as var.machine_learning_synapse_sparks"
+  value       = { for k, v in azurerm_machine_learning_synapse_spark.machine_learning_synapse_sparks : k => v.description }
 }
 output "machine_learning_synapse_sparks_identity" {
-  description = "List of identity values across all machine_learning_synapse_sparks"
-  value       = [for k, v in azurerm_machine_learning_synapse_spark.machine_learning_synapse_sparks : v.identity]
+  description = "Map of identity values across all machine_learning_synapse_sparks, keyed the same as var.machine_learning_synapse_sparks"
+  value       = { for k, v in azurerm_machine_learning_synapse_spark.machine_learning_synapse_sparks : k => v.identity }
 }
 output "machine_learning_synapse_sparks_local_auth_enabled" {
-  description = "List of local_auth_enabled values across all machine_learning_synapse_sparks"
-  value       = [for k, v in azurerm_machine_learning_synapse_spark.machine_learning_synapse_sparks : v.local_auth_enabled]
+  description = "Map of local_auth_enabled values across all machine_learning_synapse_sparks, keyed the same as var.machine_learning_synapse_sparks"
+  value       = { for k, v in azurerm_machine_learning_synapse_spark.machine_learning_synapse_sparks : k => v.local_auth_enabled }
 }
 output "machine_learning_synapse_sparks_location" {
-  description = "List of location values across all machine_learning_synapse_sparks"
-  value       = [for k, v in azurerm_machine_learning_synapse_spark.machine_learning_synapse_sparks : v.location]
+  description = "Map of location values across all machine_learning_synapse_sparks, keyed the same as var.machine_learning_synapse_sparks"
+  value       = { for k, v in azurerm_machine_learning_synapse_spark.machine_learning_synapse_sparks : k => v.location }
 }
 output "machine_learning_synapse_sparks_machine_learning_workspace_id" {
-  description = "List of machine_learning_workspace_id values across all machine_learning_synapse_sparks"
-  value       = [for k, v in azurerm_machine_learning_synapse_spark.machine_learning_synapse_sparks : v.machine_learning_workspace_id]
+  description = "Map of machine_learning_workspace_id values across all machine_learning_synapse_sparks, keyed the same as var.machine_learning_synapse_sparks"
+  value       = { for k, v in azurerm_machine_learning_synapse_spark.machine_learning_synapse_sparks : k => v.machine_learning_workspace_id }
 }
 output "machine_learning_synapse_sparks_name" {
-  description = "List of name values across all machine_learning_synapse_sparks"
-  value       = [for k, v in azurerm_machine_learning_synapse_spark.machine_learning_synapse_sparks : v.name]
+  description = "Map of name values across all machine_learning_synapse_sparks, keyed the same as var.machine_learning_synapse_sparks"
+  value       = { for k, v in azurerm_machine_learning_synapse_spark.machine_learning_synapse_sparks : k => v.name }
 }
 output "machine_learning_synapse_sparks_synapse_spark_pool_id" {
-  description = "List of synapse_spark_pool_id values across all machine_learning_synapse_sparks"
-  value       = [for k, v in azurerm_machine_learning_synapse_spark.machine_learning_synapse_sparks : v.synapse_spark_pool_id]
+  description = "Map of synapse_spark_pool_id values across all machine_learning_synapse_sparks, keyed the same as var.machine_learning_synapse_sparks"
+  value       = { for k, v in azurerm_machine_learning_synapse_spark.machine_learning_synapse_sparks : k => v.synapse_spark_pool_id }
 }
 output "machine_learning_synapse_sparks_tags" {
-  description = "List of tags values across all machine_learning_synapse_sparks"
-  value       = [for k, v in azurerm_machine_learning_synapse_spark.machine_learning_synapse_sparks : v.tags]
+  description = "Map of tags values across all machine_learning_synapse_sparks, keyed the same as var.machine_learning_synapse_sparks"
+  value       = { for k, v in azurerm_machine_learning_synapse_spark.machine_learning_synapse_sparks : k => v.tags }
 }
 
